364ec5596a8eed415cb2d292cadacd8a463b3c38
Ca-edu <c.campuzanopacheco@gmail.com>
2025-07-01 19:33:36 -0500

    "Recordando aleatorias"

--- src/Prueba/Carlos.java
+++ src/Prueba/Carlos.java
@@ -8,13 +8,22 @@ package Prueba;
  *
  * @author cecam
  */
+
+import java.util.Random;
+
 public class Carlos {
     public static void main(String[] args) {
+        int arreglo[]=new int[10];
+        Random rnd = new Random();
         for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+            
+            arreglo[i]=rnd.nextInt(50-25+1)+25;
             
         }
-        System.out.println("Hallo");
-        // TEST
+        
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println(arreglo[i]);
+        }
+//        Recordando aleatorias
     }
 }
